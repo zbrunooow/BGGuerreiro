ed3f488780fd031f721961e5c5e68cab3a9d7b48
Gabriel Lopes <zbrunooow@gmail.com>
2024-01-06 03:46:07 -0300

    fixed command execution

--- src/main/java/me/zbrunooow/bgguerreiro/listener/server/CommandExecutionListener.java
+++ src/main/java/me/zbrunooow/bgguerreiro/listener/server/CommandExecutionListener.java
@@ -12,28 +12,28 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CommandExecutionListener implements Listener {
 
-  @EventHandler(priority = EventPriority.HIGHEST)
+  @EventHandler(ignoreCancelled = true,priority = EventPriority.LOWEST)
   public void onCommand(PlayerCommandPreprocessEvent e) {
     if (!e.getPlayer().hasPermission("bgguerreiro.admin")) {
-      if (!(EventManager.getCreated().getStatus() == EventStatus.STARTED)) {
+      if (EventManager.getCreated().getStatus() != EventStatus.OFF) {
         if (BoxManager.get().getSpectators().contains(e.getPlayer())) {
-          if (!e.getMessage().startsWith("/guerreiro")) {
+          if (!(e.getMessage().startsWith("/guerreiro") || e.getMessage().startsWith("/bgguerreiro") || e.getMessage().startsWith("/warrior"))) {
             e.setCancelled(true);
+            return;
           }
-          return;
         }
 
         if (Manager.getCreated().getParticipants().contains(e.getPlayer())) {
           if (!Config.get().isComandos()) {
-            if (!e.getMessage().startsWith("/guerreiro")) {
+            if (!(e.getMessage().startsWith("/guerreiro") || e.getMessage().startsWith("/bgguerreiro") || e.getMessage().startsWith("/warrior"))) {
               e.getPlayer()
                   .sendMessage("§cOs comandos estão bloqueados durante o evento Guerreiro.");
               e.setCancelled(true);
+              return;
             }
           }
         }
       }
     }
-    return;
   }
 }
